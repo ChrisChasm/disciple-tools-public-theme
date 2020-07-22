16bd4645336b03a76bf22375d193025e76d727c2
Chris Chasm <chris@chasm.solutions>
2020-07-22 10:40:17 -0600

    plugin template part

--- parts/content-plugin-makelist.php
+++ parts/content-plugin-makelist.php
@@ -1,7 +1,7 @@
 <div>
     <h4>Make Your Own Plugin</h4>
     <ul>
-        <li><a href="https://github.com/DiscipleTools/disciple-tools-starter-plugin-template">Starter Plugin Template</a></li>
-        <li><a href="https://github.com/DiscipleTools/disciple-tools-one-page-extension">One Page Extension Template</a></li>
+        <li><a href="/plugins/plugin-starter-template/">Starter Plugin Template</a></li>
+        <li><a href="/plugins/simple-one-page-plugin-template/">Simple "One Page" Plugin Template</a></li>
     </ul>
 </div>
