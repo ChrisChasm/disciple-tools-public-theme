c12d5abc49f96ce28a45897d35e660ba9f6c839d
Chris Chasm <chris@chasm.solutions>
2020-09-15 10:34:28 -0600

    Updated sign up form

--- parts/content-news-subscribe.php
+++ parts/content-news-subscribe.php
@@ -10,7 +10,7 @@
        We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style>
 <div id="mc_embed__signup">
-    <form action="https://dtpsproject.us14.list-manage.com/subscribe/post?u=ccd7c18cc92d558aadc2caefb&amp;id=18abb73d5c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <form action="https://tools.us14.list-manage.com/subscribe/post?u=d9ad41b66865008d664ac28bf&amp;id=449bdb3570" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div class="input-group">
             <input type="email" value="" name="EMAIL" class="required email input-group-field" id="mce-EMAIL" placeholder="Email Address" />
             <div class="input-group-button">
@@ -21,10 +21,10 @@
             <div class="response" id="mce-error-response" style="display:none"></div>
             <div class="response" id="mce-success-response" style="display:none"></div>
         </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ccd7c18cc92d558aadc2caefb_18abb73d5c" tabindex="-1" value=""></div>
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d9ad41b66865008d664ac28bf_449bdb3570" tabindex="-1" value=""></div>
     </form>
 </div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 <!--End mc_embed_signup-->
 
 </div>
