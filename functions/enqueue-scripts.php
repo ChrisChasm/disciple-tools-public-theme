804a160a7540a852d65fd8292e67d71d150c3a94
corsacca <corsacca@gmail.com>
2021-01-26 18:43:52 +0100

    Remove Zume code.

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -35,18 +35,18 @@ function site_scripts() {
     $dtps_user_meta = dtps_get_user_meta( $dtps_user->ID );
 
     // main minimized scripts for loaded on all pages
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js', array( 'jquery', 'lodash', 'wp-i18n' ), filemtime( get_template_directory() . '/assets/scripts/scripts.js' ), true );
-
-    wp_register_script( 'dtps-core', get_template_directory_uri() . '/assets/scripts/api.js', array( 'jquery', 'lodash', 'wp-i18n' ), filemtime( get_theme_file_path() . '/assets/scripts/api.js' ), true );
-    wp_enqueue_script( 'dtps-core' );
-    wp_localize_script(
-        "dtps-core", "dtpsCore", array(
-            'root' => esc_url_raw( rest_url() ),
-            'nonce' => wp_create_nonce( 'wp_rest' ),
-            'theme_uri' => get_stylesheet_directory_uri(),
-            'logged_in' => is_user_logged_in(),
-        )
-    );
+//    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js', array( 'jquery', 'lodash', 'wp-i18n' ), filemtime( get_template_directory() . '/assets/scripts/scripts.js' ), true );
+//
+//    wp_register_script( 'dtps-core', get_template_directory_uri() . '/assets/scripts/api.js', array( 'jquery', 'lodash', 'wp-i18n' ), filemtime( get_theme_file_path() . '/assets/scripts/api.js' ), true );
+//    wp_enqueue_script( 'dtps-core' );
+//    wp_localize_script(
+//        "dtps-core", "dtpsCore", array(
+//            'root' => esc_url_raw( rest_url() ),
+//            'nonce' => wp_create_nonce( 'wp_rest' ),
+//            'theme_uri' => get_stylesheet_directory_uri(),
+//            'logged_in' => is_user_logged_in(),
+//        )
+//    );
 
     // lodash load
     wp_register_script( 'lodash', 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js', false, '4.17.11' );
@@ -57,56 +57,13 @@ function site_scripts() {
     wp_style_add_data( 'site-css', 'rtl', 'replace' );
 
     // script for threaded comments
-    if ( is_singular() && comments_open() && ( get_option( 'thread_comments' ) == 1 )) {
-        wp_enqueue_script( 'comment-reply' );
-    }
+//    if ( is_singular() && comments_open() && ( get_option( 'thread_comments' ) == 1 )) {
+//        wp_enqueue_script( 'comment-reply' );
+//    }
 
     // foundation styles
     wp_enqueue_style( 'foundations-icons', get_template_directory_uri() .'/assets/styles/foundation-icons/foundation-icons.css', array(), '3' );
 
-
-    /**
-     * Profile Page
-     */
-    if ( 'template-account.php' === basename( get_page_template() ) ) {
-        wp_register_script( 'lodash', 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js', false, '4.17.11' );
-        wp_enqueue_script( 'lodash' );
-
-        wp_enqueue_script( 'dtps-profile', get_template_directory_uri() . '/assets/scripts/profile.js', array( 'jquery', 'lodash', 'wp-i18n', 'dtps-core' ), filemtime( get_theme_file_path() . '/assets/scripts/profile.js' ), true );
-        wp_localize_script(
-            "dtps-profile", "dtpsProfile", array(
-                'root' => esc_url_raw( rest_url() ),
-                'theme_uri' => get_stylesheet_directory_uri(),
-                'nonce' => wp_create_nonce( 'wp_rest' ),
-                'current_user_id' => get_current_user_id(),
-                'user_profile_fields' => array(
-                    'id' => $dtps_user->data->ID,
-                    'name' => $dtps_user_meta['dtps_full_name'] ?? '',
-                    'email' => $dtps_user->data->user_email,
-                    'phone' => $dtps_user_meta['dtps_phone_number'] ?? '',
-                    'location_grid_meta' => maybe_unserialize( $dtps_user_meta['location_grid_meta'] ) ?? '',
-                    'affiliation_key' => $dtps_user_meta['dtps_affiliation_key'] ?? '',
-                    'facebook_sso_email' => $dtps_user_meta['facebook_sso_email'] ?? false,
-                    'google_sso_email' => $dtps_user_meta['google_sso_email'] ?? false,
-                ),
-                'logged_in' => is_user_logged_in(),
-            )
-        );
-    }
-
-    if ( 'template-join.php' === basename( get_page_template() ) ) {
-        wp_register_script( 'lodash', 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js', false, '4.17.11' );
-        wp_enqueue_script( 'lodash' );
-
-        wp_enqueue_script( 'dtps-join', get_template_directory_uri() . '/assets/scripts/profile.js', array( 'jquery', 'lodash', 'wp-i18n', 'dtps-core' ), filemtime( get_theme_file_path() . '/assets/scripts/profile.js' ), true );
-        wp_localize_script(
-            "dtps-join", "dtpsJoin", array(
-                'root' => esc_url_raw( rest_url() ),
-                'theme_uri' => get_stylesheet_directory_uri(),
-                'nonce' => wp_create_nonce( 'wp_rest' ),
-            )
-        );
-    }
 
 }
 add_action( 'wp_enqueue_scripts', 'site_scripts', 999 );
