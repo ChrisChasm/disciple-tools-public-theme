9dfbf7bcecbb8c3d1f5806df4b76efc65cd3a164
Chris Chasm <chris@chasm.solutions>
2021-01-28 16:17:13 -0700

    Refactored link name

--- parts/content-plugin-makelist.php
+++ parts/content-plugin-makelist.php
@@ -2,7 +2,7 @@
     <h4>Make Your Own Plugin</h4>
     <ul>
         <li><a href="/dev-docs/introduction/plugins/">Documentation</a></li>
-        <li><a href="/plugins/plugin-starter-template/">Starter Plugin Template</a></li>
+        <li><a href="/plugins/plugin-starter-template/">Plugin Starter Template</a></li>
         <li><a href="/plugin-categories/developer-tools/">Development Tools</a></li>
     </ul>
 </div>
