97a025f042ed23730f18fea10479db5315037392
Chris Chasm <chris@chasm.solutions>
2021-05-06 14:23:04 +0100

    Updated google tag manager

--- header.php
+++ header.php
@@ -12,13 +12,15 @@
   <html class="no-js"  <?php language_attributes(); ?>>
 
     <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WCRQSZT');</script>
-        <!-- End Google Tag Manager -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T5R42ERNV1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-T5R42ERNV1');
+        </script>
 
 
         <meta charset="utf-8">
