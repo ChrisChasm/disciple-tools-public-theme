87e526d2479f0171fc54263944a4c8e34bb805a8
Prykon <dariomanoukian@gmail.com>
2021-10-08 08:14:07 -0300

    Moved sidebar to left

--- single-user_documentation.php
+++ single-user_documentation.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
         <div class="grid-x grid-margin-x grid-padding-x">
 
-            <div class="cell large-4 callout show-for-small-only">
+            <div class="cell large-4 callout hide-for-small-only">
 
                 <?php get_template_part( "parts/sidebar", "user-documentation" ); ?>
 
@@ -29,12 +29,6 @@ get_header(); ?>
                     <?php get_template_part( 'parts/content', 'missing' ); ?>
 
                 <?php endif; ?>
-
-            </div>
-
-            <div class="cell large-4 callout hide-for-small-only">
-
-                <?php get_template_part( "parts/sidebar", "user-documentation" ); ?>
 
             </div>
 
