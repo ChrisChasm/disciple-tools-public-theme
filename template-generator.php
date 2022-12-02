821cb70689d8d2f2feb7bc0f063661c543d83ae6
corsacca <corsacca@gmail.com>
2022-12-02 14:14:55 +0100

    Invoice Generator

---
+++ template-generator.php
@@ -0,0 +1,77 @@
+<?php /* Template Name: Invoice generator */ ?>
+
+<?php get_header(); ?>
+
+<style>
+    #dt_generate_form {
+        max-width: 400px;
+    }
+    #dt_generate_form .input-group-addon {
+        padding: 7px; min-width: 50px; text-align: center;
+        background-color: #eee; margin-bottom: 16px;
+        border: 1px solid rgb(202, 202, 202);
+        height: 39px;
+    }
+    .page-inner-wrapper {
+        max-width: 1000px;
+    }
+
+</style>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="page-inner-wrapper" style="padding:20px">
+            <h1>Invoice Generator</h1>
+            <p>
+                Input the values below to help generate an invoice that can be used to donate to Disciple.Tools.
+            </p>
+            <p style='padding: 10px; background-color: #90b4d066; border-radius: 2px; color:#3f729b'>
+                <strong>Note:</strong> The values you enter in the form below are not saved or viewed by Disciple.Tools. This tool is for your own use to generate an invoice for a donation.
+            </p>
+
+            <form id="dt_generate_form" action="generated-invoice">
+
+                <label>
+                    Invoice Date
+                    <input type="date" name="date" required value="<?php echo esc_html( gmdate( 'Y-m-d' ) ); ?>">
+                </label>
+                <label>
+                    Organization Name
+                    <input type="text" name="org" required>
+                </label>
+                <label>
+                    Attention Name
+                    <input type="text" name="attention" required>
+                </label>
+                <label>
+                    Donation Amount
+                    <div class="input-group" style="display: flex">
+                        <span class="input-group-addon">$</span>
+                        <input type="number" name="amount" required>
+                    </div>
+                </label>
+                <label>
+                    Street Address
+                    <input type="text" name="address" required>
+                </label>
+                <label>
+                    City
+                    <input type="text" name="city" required>
+                </label>
+                <label>
+                    State
+                    <input type="text" name="state" required>
+                </label>
+                <label>
+                    Postal Code
+                    <input type="text" name="zip" required>
+                </label>
+                <button type="submit" class="button">Generate Invoice</button>
+            </form>
+
+
+        </div>
+    </main><!-- .site-main -->
+    <?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+<?php get_footer(); ?>
---
+++ template-invoice.php
@@ -0,0 +1,119 @@
+<?php /* Template Name: Invoice */ ?>
+
+<?php //get_header(); ?>
+
+
+<style>
+    #main {
+        max-width:1000px;
+        padding: 20px;
+        font-size: .9rem;
+    }
+    #main h1 {
+        color: #3f729b;
+    }
+    table {
+        border-collapse: collapse;
+
+    }
+    table th {
+        background-color: #3f729b;
+        color: white;
+    }
+    table th, td {
+        text-align: left;
+        padding: 6px 10px;
+        border: 1px solid black;
+    }
+    table tbody td {
+        color: #727272;
+        font-size: .9rem;
+    }
+    .address-color {
+        color: #727272;
+    }
+    #main {
+        color: #727272;
+    }
+</style>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="page-inner-wrapper">
+            <h1>Invoice</h1>
+            <?php
+                $get = dt_recursive_sanitize_array( $_GET );
+                $amount = $get['amount'] ?? 0;
+                $organization_name = $get['org'] ?? '';
+                $attention_name = $get['attention'] ?? '';
+                $address = $get['address'] ?? '';
+                $city = $get['city'] ?? '';
+                $state = $get['state'] ?? '';
+                $zip = $get['zip'] ?? '';
+                $date = $get['date'] ?? '';
+            ?>
+
+            <div style="display: flex; justify-content: space-between">
+                <div>
+                    <p class="address-color">
+                        <?php if ( !empty( $attention_name ) ) : ?>
+                            <?php echo esc_html( $attention_name ); ?>
+                            <br>
+                        <?php endif; ?>
+                        <?php echo esc_html( $organization_name ); ?>
+                        <br>
+                        <?php echo esc_html( $address ); ?>
+                        <br>
+                        <?php echo esc_html( $city ); ?>, <?php echo esc_html( $state ); ?> <?php echo esc_html( $zip ); ?>
+                    </p>
+                </div>
+                <div>
+                    <div style="text-align: right">
+                        <div style="text-align: left">
+                            <img style="height:50px" src="<?php echo esc_html( get_template_directory_uri() . '/assets/images/dt-logo-medium.png' ) ?>"/>
+                            <p>
+                                Disciple.tools c/o Gospel Ambition
+                                <br>
+                                PO Box 325
+                                <br>
+                                Mooreland OK 73852
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <p>
+                <strong>Invoice Date</strong>: <?php echo esc_html( $date ); ?>
+            </p>
+
+            <table style="width: 100%">
+                <thead>
+                    <tr>
+                        <th>Item Description</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Disciple.Tools Donation</td>
+                        <td>$<?php echo esc_html( number_format( $amount ) ) ?></td>
+                    </tr>
+                    <tr style="font-weight: bold">
+                        <td>Total</td>
+                        <td>$<?php echo esc_html( number_format( $amount ) ) ?></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <p style="padding: 10px; background-color: #90b4d066; border-radius: 2px; color:#3f729b">
+                <strong>Note:</strong> This invoice is presented as a tool to assist you with making a donation to help with the development and support of Disciple.Tools. It does not represent an amount owed or bill.
+            </p>
+
+
+        </div>
+    </main><!-- .site-main -->
+
+</div><!-- .content-area -->
+<?php //get_footer(); ?>
