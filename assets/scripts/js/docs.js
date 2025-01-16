4a382a37091f08b1d6e0588e2ccff9363ac58bfa
corsacca <corsacca@gmail.com>
2025-01-16 10:51:33 +0100

    Docs masonry

--- assets/scripts/js/docs.js
+++ assets/scripts/js/docs.js
@@ -11,3 +11,15 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("post-main").style.marginLeft= "0";
 }
+
+if (window.location.href.indexOf("docs") > -1) {
+  $('.betterdocs-nested-category-wrapper').on('click', function () {
+    setTimeout(() => {
+      $('.masonry').masonry({
+        itemSelector: ".betterdocs-single-category-wrapper",
+        percentPosition: true,
+        gutter: 15
+      })
+    }, 200);
+  })
+}
